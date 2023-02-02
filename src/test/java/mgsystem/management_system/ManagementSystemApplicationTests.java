fb782aa1449f084d2ef87932f4faca5d872af0b2
Mavis <llingchen.27@gmail.com>
2023-02-02 16:04:15 +0800

    create a new project and create a database in PostgreSQL

---
+++ src/test/java/mgsystem/management_system/ManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package mgsystem.management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
