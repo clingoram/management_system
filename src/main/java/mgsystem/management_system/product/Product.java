fd3361399d0ec34a2891cc42d2101c7a13469535
Mavis <llingchen.27@gmail.com>
2023-02-03 14:30:27 +0800

    建立product table、g3m/4ThymeLeaf模板

---
+++ src/main/java/mgsystem/management_system/product/Product.java
@@ -0,0 +1,81 @@
+package mgsystem.management_system.product;
+
+import jakarta.persistence.*;
+
+// Create table.
+
+@Entity
+@Table(name="product")
+public class Product {
+    @Id
+    @SequenceGenerator(
+            name = "product_sequence",
+            sequenceName = "product_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "product_sequence"
+    )
+
+    // property
+    private Long id;
+    private String name;
+    private float price;
+    private int order_number;
+
+    // Constructor 1(JPA預設).
+    public Product(){
+
+    }
+    // Constructor 2(用來建立跟資料庫有關的資料表相關欄位等資料)
+    public Product(Long id,String name,float price,int orderNumber) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.order_number = orderNumber;
+    }
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    // Getter
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getPrice(){
+        return price;
+    }
+    public int getOrder_number(){
+        return order_number;
+    }
+
+    // Setter
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setOrder_number(int orderNumber){
+        this.order_number = orderNumber;
+    }
+
+    @Override
+    public String toString(){
+        return
+                "Product {" + "id = " + id +", name = " + name +",price= "+ price+ " number = " + order_number +"}";
+    }
+}
