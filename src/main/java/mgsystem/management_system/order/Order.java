76691bbe2126af7aacabfce3e206797ff5797366
Mavis <llingchen.27@gmail.com>
2023-05-31 15:43:28 +0800

    folder order need to connect to Redis

--- src/main/java/mgsystem/management_system/order/Order.java
+++ src/main/java/mgsystem/management_system/order/Order.java
@@ -1,36 +1,43 @@
-//package mgsystem.management_system.order;
-//
-//import com.fasterxml.jackson.annotation.JsonBackReference;
-//import jakarta.persistence.*;
-//import mgsystem.management_system.product.Product;
-//
+package mgsystem.management_system.order;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import mgsystem.management_system.product.Product;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPooled;
+
+import java.util.HashMap;
+import java.util.Map;
+
 //@Entity
 //@Table(name = "order")
-//public class Order {
-//
-//    @Id
-//    @GeneratedValue
-//    private long id;
-//
-//    @JsonBackReference
-//    @ManyToOne(cascade = CascadeType.ALL,targetEntity = Product.class)
-//    @JoinColumn(name = "product_id",columnDefinition = "jsonb")
-//    private Product product;
-//
-//    @Column(name="cost")
-//    private  int cost;
-//
-//    @Column(name="note")
-//    private String note;
-//
-//    public Order(){
-//    }
-//
-//    public Order(long id,int cost,String note){
-//        this.id = id;
-//        this.cost = cost;
-//        this.note = note;
-//    }
+
+public class Order {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @JsonBackReference
+    @ManyToOne(cascade = CascadeType.ALL,targetEntity = Product.class)
+    @JoinColumn(name = "product_id",columnDefinition = "jsonb")
+    private Product product;
+
+    @Column(name="cost")
+    private  int cost;
+
+    @Column(name="note")
+    private String note;
+
+    public Order(){
+    }
+
+    public Order(long id,int cost,String note){
+        this.id = id;
+        this.cost = cost;
+        this.note = note;
+    }
 //
 //    public setCost(){
 //
@@ -38,4 +45,4 @@
 //    public getCost(){
 //
 //    }
-//}
+}
---
+++ src/main/java/mgsystem/management_system/order/OrderService.java
@@ -0,0 +1,13 @@
+package mgsystem.management_system.order;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class OrderService {
+    @Autowired
+    private OrderRepository order;
+
+    public void setOrder(){
+
+    }
+
+}
