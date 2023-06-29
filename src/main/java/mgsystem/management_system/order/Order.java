b2a8e64c275187e3e675794382210ed3a0e55840
Mavis <llingchen.27@gmail.com>
2023-06-29 15:36:36 +0800

    add comments of folder order

--- src/main/java/mgsystem/management_system/order/Order.java
+++ src/main/java/mgsystem/management_system/order/Order.java
@@ -13,36 +13,36 @@ import java.util.Map;
 //@Entity
 //@Table(name = "order")
 
-public class Order {
-
-    @Id
-    @GeneratedValue
-    private long id;
-
-    @JsonBackReference
-    @ManyToOne(cascade = CascadeType.ALL,targetEntity = Product.class)
-    @JoinColumn(name = "product_id",columnDefinition = "jsonb")
-    private Product product;
-
-    @Column(name="cost")
-    private  int cost;
-
-    @Column(name="note")
-    private String note;
-
-    public Order(){
-    }
-
-    public Order(long id,int cost,String note){
-        this.id = id;
-        this.cost = cost;
-        this.note = note;
-    }
+//public class Order {
 //
-//    public setCost(){
+//    @Id
+//    @GeneratedValue
+//    private long id;
 //
+//    @JsonBackReference
+//    @ManyToOne(cascade = CascadeType.ALL,targetEntity = Product.class)
+//    @JoinColumn(name = "product_id",columnDefinition = "jsonb")
+//    private Product product;
+//
+//    @Column(name="cost")
+//    private  int cost;
+//
+//    @Column(name="note")
+//    private String note;
+//
+//    public Order(){
 //    }
-//    public getCost(){
 //
+//    public Order(long id,int cost,String note){
+//        this.id = id;
+//        this.cost = cost;
+//        this.note = note;
 //    }
-}
+////
+////    public setCost(){
+////
+////    }
+////    public getCost(){
+////
+////    }
+//}
--- src/main/java/mgsystem/management_system/order/OrderService.java
+++ src/main/java/mgsystem/management_system/order/OrderService.java
@@ -2,12 +2,12 @@ package mgsystem.management_system.order;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class OrderService {
-    @Autowired
-    private OrderRepository order;
-
-    public void setOrder(){
-
-    }
-
-}
+//public class OrderService {
+//    @Autowired
+//    private OrderRepository order;
+//
+//    public void setOrder(){
+//
+//    }
+//
+//}
