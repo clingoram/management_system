764511c34eb157f6b45f14b0c0e1391d6b40ee55
Mavis <llingchen.27@gmail.com>
2023-06-30 15:41:24 +0800

    Create a login html file and a management folder for login and authorization.

---
+++ src/main/java/mgsystem/management_system/management/Management.java
@@ -0,0 +1,27 @@
+//package mgsystem.management_system.management;
+//
+//import jakarta.persistence.*;
+//
+//
+//@Entity
+//@Table(name="management")
+//public class Management {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private long id;
+//    @Column(name = "account",length = 20)
+//    private String account;
+//
+//    @Column(name = "password")
+//    private String password;
+//
+//    @Column(name = "salt")
+//    private String salt;
+//
+//    @Column(name = "last_login")
+//    private String last_login;
+//
+//    @Column
+//    private String role;
+//}
