003ce0f73f341910a75a37cf2657c8156e6dc5c6
Mavis <llingchen.27@gmail.com>
2023-07-07 15:44:21 +0800

    add security,not done

--- src/main/java/mgsystem/management_system/management/Management.java
+++ src/main/java/mgsystem/management_system/management/Management.java
@@ -1,27 +1,28 @@
-//package mgsystem.management_system.management;
-//
-//import jakarta.persistence.*;
-//
-//
-//@Entity
-//@Table(name="management")
-//public class Management {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private long id;
-//    @Column(name = "account",length = 20)
-//    private String account;
-//
-//    @Column(name = "password")
-//    private String password;
-//
-//    @Column(name = "salt")
-//    private String salt;
-//
-//    @Column(name = "last_login")
-//    private String last_login;
-//
-//    @Column
-//    private String role;
-//}
+package mgsystem.management_system.management;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+@Entity
+@Table(name="management")
+abstract class Management implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "account",length = 20)
+    private String account;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "salt")
+    private String salt;
+
+    @Column(name = "last_login")
+    private String last_login;
+
+    @Column
+    private String role;
+}
